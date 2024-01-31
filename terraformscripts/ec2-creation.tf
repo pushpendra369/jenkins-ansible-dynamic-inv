93119049088bb95f97f72863577c5d72ebcb21e4
Pushpendra Kumar Raniwal <151851010+pushpendra369@users.noreply.github.com>
2024-01-31 17:20:11 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,12 +3,12 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "AWSEC2Instance"{
-ami = "ami-0d3f444bc76de0a79"
+ami = "ami-00952f27cf14db9cd"
 instance_type = "t2.micro"
-key_name = "devopsoctbatch"
+key_name = "newserver"
 security_groups = ["launch-wizard-2"]
 tags={
-Name = "tomcatservers"
+Name = "tomcatserversnew"
 }
 }
 
